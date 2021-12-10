7089be4904d125ff62dd6e0dccefb60f81400053
fabiof2x <fabioar@gmail.com>
2021-12-10 15:58:19 -0300

    Utilizando proporções em medidas

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 
-import { Container, Title } from './styles';
+import {
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  Photo,
+  User,
+  UserGreeting,
+  UserName
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo
+              source={{ uri: 'https://github.com/fabiof2x.png' }}
+            />
+            <User>
+              <UserGreeting>Olá,</UserGreeting>
+              <UserName>Fábio</UserName>
+            </User>
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 } 
