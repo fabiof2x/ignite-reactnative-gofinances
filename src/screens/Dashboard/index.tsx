2735812bcfd1982d4d942807c1b198390ee4b061
fabiof2x <fabioar@gmail.com>
2021-12-10 16:13:57 -0300

    Utilizando Ícones

--- src/screens/Dashboard/index.tsx
+++ src/screens/Dashboard/index.tsx
@@ -8,7 +8,8 @@ import {
   Photo,
   User,
   UserGreeting,
-  UserName
+  UserName,
+  Icon
 } from './styles';
 
 export function Dashboard() {
@@ -25,6 +26,7 @@ export function Dashboard() {
               <UserName>Fábio</UserName>
             </User>
           </UserInfo>
+          <Icon name="power" />
         </UserWrapper>
       </Header>
     </Container>
